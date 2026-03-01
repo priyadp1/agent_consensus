d336d9fba1a37c3b2b0ab3f2ed091773fe13566f
Prisha Priyadarshini <150696909+priyadp1@users.noreply.github.com>
2026-03-01 16:04:23 -0500

    Modified random_baselines.py

--- random_baselines.py
+++ random_baselines.py
@@ -31,10 +31,16 @@ def calculate_true_disagreement_baseline(options_counts):
         sum_baseline = sum_baseline + random_baseline
     true_disagreement_baseline = sum_baseline / length
     return true_disagreement_baseline
+
+
 if __name__ == "__main__":
-    results_dir = "results/GlobalOpinionsQA/agent_names/20_rounds/gpt-4.1-fam/agents_3_questions_2556/anonymous"
+    results_dir = "results/GlobalOpinionsQA/agent_names/gpt-4.1-fam/agents_3_questions_2556/adversarial/named"
     option_counts = count_options(results_dir)
     pairwise_baseline = calculate_pairwise_baseline(list(option_counts.values()))
     true_disagreement_baseline = calculate_true_disagreement_baseline(list(option_counts.values()))
+    length = len(option_counts)
+    options_counts = list(option_counts.values())
+    print(f"Total questions: {length}")
+    print(f"Option Counts: {options_counts}")
     print(f"Pairwise Baseline: {pairwise_baseline:.4f}")
     print(f"True Disagreement Baseline: {true_disagreement_baseline:.4f}")

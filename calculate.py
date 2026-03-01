1ff14a2cc7a5cdf5be8f2196ef01d23a0c575331
Prisha Priyadarshini <150696909+priyadp1@users.noreply.github.com>
2026-03-01 15:52:00 -0500

    Wrote random baseline code

--- calculate.py
+++ calculate.py
@@ -17,14 +17,23 @@ def calculate_pairwise_baseline(options_counts):
     sum_baseline = 0
     for i in range(length):
         random_baseline = (options_counts[i] - 1) / options_counts[i]
-        sum_baseline += random_baseline
+        sum_baseline = sum_baseline + random_baseline
     pairwise_baseline = sum_baseline / length
     return pairwise_baseline
 
 def calculate_true_disagreement_baseline(options_counts):
-    pass
+    models = 3
+    sum_baseline = 0
+    length = len(options_counts)
+    for i in range(length):
+        random_baseline = 1 - (1 / options_counts[i]) ** (models - 1)
+        sum_baseline = sum_baseline + random_baseline
+    true_disagreement_baseline = sum_baseline / length
+    return true_disagreement_baseline
 if __name__ == "__main__":
     results_dir = "results/GlobalOpinionsQA/agent_names/20_rounds/gpt-4.1-fam/agents_3_questions_2556/anonymous"
     option_counts = count_options(results_dir)
     pairwise_baseline = calculate_pairwise_baseline(list(option_counts.values()))
+    true_disagreement_baseline = calculate_true_disagreement_baseline(list(option_counts.values()))
     print(f"Pairwise Baseline: {pairwise_baseline:.4f}")
+    print(f"True Disagreement Baseline: {true_disagreement_baseline:.4f}")
